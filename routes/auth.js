be65c408ff481e30e37794788fb99be59fb23e2a
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-16 18:29:28 +0200

    iteration 2 done

--- routes/auth.js
+++ routes/auth.js
@@ -11,6 +11,14 @@ router.get('/signup', (req, res) => {
 //POST /signup
 router.post('/signup', (req, res) => {
     const { username, password } = req.body;
+
+    //Checking if both fields contain data
+    if (!username || !password){
+        res.render('auth/signup', {errorMessage: 'Please input both username and password'});
+        return; 
+    }
+
+    //creating a new user (bcrypt for password)
     bcrypt
         .genSalt(saltRounds)
         .then(salt => bcrypt.hash(password, salt))
@@ -30,6 +38,38 @@ router.post('/signup', (req, res) => {
 //GET /login 
 router.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+//POST /login
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    
+    //Checking if both fields contain data
+    if (!username || !password){
+        res.render('auth/signup', {errorMessage: 'Please input both username and password'});
+        return;
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            if (!user) {
+                res.render('auth/login', {errorMessage: 'This account does not exist'})
+                return;
+            }
+            else if (bcrypt.compareSync(password, user.password)) {
+                req.session.currentUser = user;
+                res.redirect('/auth/profile');
+            }
+            else {
+                res.render('auth/login', {errorMessage: 'Incorrect password'})
+            }
+        })
+        .catch(err => console.log(err));
+});
+
+//GET /userProfile
+router.get('/profile', (req, res) => {
+    res.render('auth/userProfile', { userInSession: req.session.currentUser });
 });
 
 module.exports = router; 
